9350d9f052d7c1c7d8b86da7ba97a4bb44cd9b5c
BTF-dev <btfcasheb@gmail.com>
2026-03-23 18:06:38 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import '../styles/globals.css'
-import { inter } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import { Providers } from './providers'
 
-const inter = inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Albion Crafting Calculator',
