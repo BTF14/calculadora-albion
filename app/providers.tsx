08b3910923c4da7d435a3cee19c534be41d5dc26
BTF-dev <btfcasheb@gmail.com>
2026-03-23 18:47:41 -0400

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,16 +1,6 @@
 'use client'
-
-import { SessionProvider } from 'next-auth/react'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { useState } from 'react'
+import { SessionProvider } from "next-auth/react"
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient())
-  return (
-    <SessionProvider>
-      <QueryClientProvider client={queryClient}>
-        {children}
-      </QueryClientProvider>
-    </SessionProvider>
-  )
+  return <SessionProvider>{children}</SessionProvider>
 }
